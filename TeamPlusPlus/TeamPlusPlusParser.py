406e0f2dc3d8de8f9b26cbf0f31eeb0a1ec80755
Santiago Limas <santiago@kinedu.com>
2021-04-03 23:27:58 -0600

    added new rules

--- TeamPlusPlus/TeamPlusPlusParser.py
+++ TeamPlusPlus/TeamPlusPlusParser.py
@@ -4,23 +4,24 @@ sys.path.insert(0, '.')
 from sly import Parser
 from TeamPlusPlusLexer import TeamPlusPlusLexer
 
+""" Falta while, for, hacer pruebas, eliminar recursividad """
+
 class TeamPlusPlusParser(Parser):
     contains_error = False
     tokens = TeamPlusPlusLexer.tokens
 
+    start = 'program'
+
     precedence = (
-        ('left', PLUS, MINUS),
-        ('left', MULTIPLY, DIVIDE),
+        ('left', '+', '-'),
+        ('left', '*', '/'),
         )
 
     def __init__(self):
         self.names = { }
 
-    @_('PROGRAM ID SEMICOLON funciones')
-    def program(self, p):
-        pass
-
-    @_('PROGRAM ID SEMICOLON globals funciones')
+    @_('PROGRAM ID ";" funciones', 
+      'PROGRAM ID ";" globals funciones main')
     def program(self, p):
         pass
 
@@ -28,47 +29,28 @@ class TeamPlusPlusParser(Parser):
     def globals(self, p):
         pass
 
-    @_('identificadores tipo SEMICOLON')
+    @_('identificadores tipo ";"', 
+      'identificadores tipo ";" declaraciones')
     def declaraciones(self, p):
         pass
 
-    @_('identificadores tipo SEMICOLON declaraciones')
-    def declaraciones(self, p):
-        pass
-
-    @_('ID')
+    @_('ID', 'ID "," identificadores')
     def identificadores(self, p):
         pass
 
-    @_('ID COMMA identificadores')
-    def identificadores(self, p):
-        pass
-
-    @_('INT_TYPE')
+    @_('INT_TYPE', 'FLOAT_TYPE', 'CHAR_TYPE')
     def tipo(self, p):
         pass
 
-    @_('FLOAT_TYPE')
-    def tipo(self, p):
-        pass
-
-    @_('funcion')
+    @_('funcion', 'funcion funciones')
     def funciones(self, p):
         pass
 
-    @_('funcion funciones')
-    def funciones(self, p):
-        pass
-
-    @_('FUNC ID LPAR parametros RPAR ARROW tipo LBRACKET RBRACKET')
+    @_('FUNC ID "(" parametros ")" ARROW tipo "{" "}"')
     def funcion(self, p):
         pass
 
-    @_('ID tipo')
-    def parametros(self, p):
-        pass
-
-    @_('ID tipo COMMA parametros')
+    @_('ID tipo', 'ID tipo "," parametros')
     def parametros(self, p):
         pass
 
@@ -76,9 +58,113 @@ class TeamPlusPlusParser(Parser):
     def parametros(self, p):
         pass
 
+    #Epsilon
     @_('')
     def epsilon(self, p):
         pass
+
+    #Main
+    @_('MAIN "(" ")" bloque')
+    def main(self, p):
+        pass
+
+    #Bloque
+    @_('"{" bloque2 "}"')
+    def bloque(self, p):
+        pass
+
+    @_('epsilon', 'estatuto', 'estatuto bloque2')
+    def bloque2(self, p):
+        pass
+    
+    #Expresion
+    @_('exp expresion2')
+    def expresion(self, p):
+        pass
+
+    @_('expresion3 exp', 'epsilon')
+    def expresion2(self, p):
+        pass
+
+    @_('LT', 'GT', 'LTEQUAL', 'GTEQUAL', 'NOTEQUAL', 'EQUAL')
+    def expresion3(self, p):
+        pass
+
+    #Exp
+    @_('termino exp2')
+    def exp(self, p):
+        pass
+
+    @_('exp3 exp', 'epsilon')
+    def exp2(self, p):
+        pass
+    
+    @_('"+"', '"-"')
+    def exp3(self, p):
+        pass
+
+    #Termino
+    @_('factor termino2')
+    def termino(self, p):
+        pass
+
+    @_('termino3 termino', 'epsilon')
+    def termino2(self, p):
+        pass
+    
+    @_('"*"', '"/"')
+    def termino3(self, p):
+        pass
+
+    #Factor
+    """Incomplete"""
+
+    @_('"(" expresion ")"', 'epsilon')
+    def factor(self, p):
+        pass
+
+    #Estatuto
+    @_('asignacion', 'escritura', 'lectura', 'condicion')
+    def estatuto(self, p):
+        pass
+
+    #Asignacion
+    @_('ID ASSIGN expresion')
+    def asignacion(self, p):
+        pass
+
+    #Escritura
+    @_('PRINT "(" ID ")"')
+    def escritura(self, p):
+        pass
+
+    #Lectura
+    @_('INPUT "(" ID ")"')
+    def lectura(self, p):
+        pass
+
+    #Condicion
+    @_('IF expresionesCond bloque condicion2')
+    def condicion(self, p):
+        pass
+
+    @_('expresion expresionesCond2')
+    def expresionesCond(self, p):
+        pass
+
+    @_('AND expresionesCond', 'OR expresionesCond', 'epsilon')
+    def expresionesCond2(self, p):
+        pass
+
+    @_('ELSE bloque', 'epsilon')
+    def condicion2(self, p):
+        pass
+
+    
+
+    #While
+
+    #For
 
 
     def error(self, p):
