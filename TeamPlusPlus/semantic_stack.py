d3cd29734d03e52fe5b0b9ec5c58644ad7a37ee2
diazhector98 <diazhector98@gmail.com>
2021-04-19 15:02:27 -0500

    creando clase semantic stack que maneja las pilas de la semantica

---
+++ TeamPlusPlus/semantic_stack.py
@@ -0,0 +1,18 @@
+
+"""
+Clase para manejar las pilas de la semántica. Se utiliza para generar los cuadruplos
+
+
+"""
+
+class SemanticStack:
+    operators = []
+    operands = []
+    types = []
+
+    def push_operand(self, operand, type):
+        self.operands.append(operand)
+        self.types.append(type)
+    
+    def push_operator(self, operator):
+        self.operator.append(operator)
