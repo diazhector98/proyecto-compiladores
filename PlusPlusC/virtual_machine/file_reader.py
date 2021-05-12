0c4879ecb5f165e236306fe6676bf8c4a93cf51a
diazhector98 <diazhector98@gmail.com>
2021-05-12 03:12:51 -0500

    creando tabla de constantes en el parseo de la vm

--- PlusPlusC/virtual_machine/file_reader.py
+++ PlusPlusC/virtual_machine/file_reader.py
@@ -3,6 +3,43 @@
 class FileReader:
     def __init__(self, filename):
         self.file = open(filename, "r")
-        self.contents = self.file.read()
-        print(self.contents)
+        self.text = self.file.read()
+        self.process()
+
+    def process(self):
+        contents = self.text.split('%%')
+        functions_text = contents[0]
+        constants_text = contents[1]
+        quadruples_text = contents[2]
+
+        self.process_functions_text(functions_text)
+        constants_table = self.process_constants_text(constants_text)
+        self.process_quadruples_text(quadruples_text)
+    
+    def process_functions_text(self, text):
+        lines = text.split('\n')
+        for l in lines:
+            pass
+
+    def process_constants_text(self, text):
+        lines = text.split('\n')
+        lines = [line for line in lines if line != '']
+        table = dict()
+        def process_line(line):
+            [string_address, string_value] = line.split(' ')
+            address = int(string_address)
+            # TODO: Pensar en como determinar tipo de valor (por ahora convierte todos a float)
+            value = float(string_value)
+            table[address] = value
+
+        for l in lines:
+            process_line(l)
+        
+        return table
+
+    def process_quadruples_text(self, text):
+        lines = text.split('\n')
+        for l in lines:
+            pass
+        
     
