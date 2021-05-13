7268d921d275d27163674d26ca5557d91efb984c
diazhector98 <diazhector98@gmail.com>
2021-05-13 15:43:44 -0700

    creando archivos para manejo de memoria de la máquina virtual

--- PlusPlusC/virtual_machine/virtual_machine.py
+++ PlusPlusC/virtual_machine/virtual_machine.py
@@ -1,5 +1,6 @@
 from virtual_machine.file_reader import FileReader
-
+from virtual_machine.memory.virtual_machine_memory import VirtualMachineMemory
 class VirtualMachine:
     def __init__(self, filename):
         [functions, constants, quadruples] = FileReader(filename)
+        self.memory = VirtualMachineMemory()
