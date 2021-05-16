13282914341515f3923be190b03c0549d4a4c969
diazhector98 <diazhector98@gmail.com>
2021-05-16 12:28:37 -0700

    escribiendo -1 si una direccion es None

--- PlusPlusC/compiler/semantic/common/quadruple.py
+++ PlusPlusC/compiler/semantic/common/quadruple.py
@@ -8,4 +8,7 @@ class Quadruple:
     temp_result: str
 
     def __str__(self) -> str:
-        return "{:<12} {:<10} {:<10} {:<10}".format(self.operator.name, str(self.left_operand), str(self.right_operand), str(self.temp_result))
+        left_operand = self.left_operand if self.left_operand != None else -1
+        right_operand = self.right_operand if self.right_operand != None else -1
+        result = self.temp_result if self.temp_result != None else -1
+        return "{:<12} {:<10} {:<10} {:<10}".format(self.operator.name, str(left_operand), str(right_operand), str(result))
