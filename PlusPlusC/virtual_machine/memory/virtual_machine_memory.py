4ffed016a5b383e1c68fa52825db9a0af0747ffa
Santiago Limas <santiago@kinedu.com>
2021-05-16 18:30:44 -0500

    modificando limite superior en bloques de memoria vm

--- PlusPlusC/virtual_machine/memory/virtual_machine_memory.py
+++ PlusPlusC/virtual_machine/memory/virtual_machine_memory.py
@@ -9,15 +9,17 @@ class VirtualMachineMemory:
     def __init__(self):
         memory_size = 20000
         self.block_size = memory_size // 4
-        self.gloabl_block = VirtualMachineMemoryBlock(0, self.block_size)
-        self.local_block = VirtualMachineMemoryBlock(self.block_size, self.block_size)
-        self.temp_block = VirtualMachineMemoryBlock(self.block_size * 2, self.block_size)
+        self.gloabl_block = VirtualMachineMemoryBlock(0, self.block_size - 1)
+        self.local_block = VirtualMachineMemoryBlock(self.block_size, self.block_size - 1)
+        self.temp_block = VirtualMachineMemoryBlock(self.block_size * 2, self.block_size - 1)
         self.constants_block = VirtualMachineMemoryBlock(self.block_size * 3, self.block_size)
 
     def write(self, address, value):
+        #pendiente
         pass
 
     def read(self, address):
+        #pendiente
         pass
 
     def get_block_type(self, address):
