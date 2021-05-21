20de96cc695154fc3c69edd589ae82f65c73b6bf
diazhector98 <diazhector98@gmail.com>
2021-05-21 16:56:23 -0700

    quitando prints

--- PlusPlusC/compiler/semantic/memory/virtual_memory_block.py
+++ PlusPlusC/compiler/semantic/memory/virtual_memory_block.py
@@ -8,17 +8,11 @@ Clase para manejar un bloque de memoria (e.g globales)
 class VirtualMemoryBlock:
     def __init__(self, start_address, size):
         partition_size = size // 4
-        self.partition_size = partition_size
         self.start_address = start_address
         self.int_partition = VirtualMemoryBlockPartition(start_address, partition_size - 1)
         self.float_partition = VirtualMemoryBlockPartition(start_address + partition_size, partition_size - 1)
         self.char_partition = VirtualMemoryBlockPartition(start_address + partition_size * 2, partition_size - 1)
         self.bool_partition = VirtualMemoryBlockPartition(start_address+ partition_size * 3, partition_size - 1)
-
-        print("int memory first address", start_address)
-        print("float memory first address", start_address + partition_size)
-        print("char memory first address", start_address + partition_size * 2)
-        print("bool memory first address", start_address+ partition_size * 3)
 
     def create_address(self, t):
         if t == VarType.INT:
@@ -40,9 +34,4 @@ class VirtualMemoryBlock:
         return self.char_partition.create_address()
 
     def create_bool_address(self):
-        print("Creating bool address")
-        addr = self.bool_partition.create_address()
-        print(addr)
-        print(self.partition_size)
-        print(self.bool_partition.start_address)
-        return addr
+        return self.bool_partition.create_address()
