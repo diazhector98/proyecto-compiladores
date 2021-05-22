ac69c8ae88bdeb8e9dc46d8d205d5874671c5884
diazhector98 <diazhector98@gmail.com>
2021-05-21 17:14:47 -0700

    haciendo bash file para compilar y ejecutar

--- PlusPlusC/run.py
+++ PlusPlusC/run.py
@@ -4,6 +4,6 @@ sys.path.insert(0, '.')
 from virtual_machine.file_reader import FileReader
 from virtual_machine.virtual_machine import VirtualMachine
 
-file_path = 'virtual_machine/test.txt'
+file_path = 'output.txt'
 virtual_machine = VirtualMachine(file_path)
 virtual_machine.run()
