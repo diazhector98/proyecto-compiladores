7d75470b349befc4e018bc7f817616e4ffe95c26
diazhector98 <diazhector98@gmail.com>
2021-05-24 17:41:47 -0700

    descartando uso de dataclass para activation record

--- PlusPlusC/virtual_machine/activation_record.py
+++ PlusPlusC/virtual_machine/activation_record.py
@@ -3,12 +3,17 @@ from virtual_machine.common.function import Function
 from virtual_machine.memory.virtual_machine_memory_block import VirtualMachineMemoryBlock
 from virtual_machine.memory.block_type import BlockType
 
-@dataclass
 class ActivationRecord:
     function: Function = None
     current_quad_index: int = 0
-    local_block: VirtualMachineMemoryBlock = VirtualMachineMemoryBlock(5000, 5000)
-    temp_block: VirtualMachineMemoryBlock = VirtualMachineMemoryBlock(10000, 5000)
+    local_block: VirtualMachineMemoryBlock
+    temp_block: VirtualMachineMemoryBlock
+
+    def __init__(self, function=None, current_quad_index = 0):
+        self.function = function
+        self.current_quad_index = current_quad_index
+        self.local_block: VirtualMachineMemoryBlock = VirtualMachineMemoryBlock(5000, 5000)
+        self.temp_block: VirtualMachineMemoryBlock = VirtualMachineMemoryBlock(10000, 5000)
 
     def write(self, address, value):
         if address >= 5000 and address < 10000:
