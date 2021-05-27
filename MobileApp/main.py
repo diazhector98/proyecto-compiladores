e5ef4c9e298a30551c81bf41e3d009fca0b84614
Santiago Limas <santiago@kinedu.com>
2021-05-27 00:52:58 -0500

    agregando UI

--- MobileApp/main.py
+++ MobileApp/main.py
@@ -1,6 +1,12 @@
+import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.lang import Builder
 
+Builder.load_file("compiladores.kv")
 
 class Compiladores(Widget):
     pass
