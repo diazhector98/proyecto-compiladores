4c6d46e1eb8f3bee02296b2dce0dccb48194bf93
diazhector98 <diazhector98@gmail.com>
2021-05-28 16:07:41 -0700

    conectando botones de app de ios a acciones

--- ios/PlusPlusC/PlusPlusC/ViewController.swift
+++ ios/PlusPlusC/PlusPlusC/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var compileButton: UIButton!
+    @IBOutlet weak var runButton: UIButton!
+    
+    @IBOutlet weak var codeInputTextView: UITextView!
+    @IBOutlet weak var outputTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
     }
 
-
+    @IBAction func compileButtonPressed(_ sender: Any) {
+        let codeInput = codeInputTextView.text
+        print(codeInput!)
+    }
+    
+    @IBAction func runButtonPressed(_ sender: Any) {
+    }
+    
+    
+    
+    
 }
 
