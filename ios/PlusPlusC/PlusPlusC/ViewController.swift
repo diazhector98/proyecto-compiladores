55ff8c18fcb1535a2ca6799d5693a74ff9b599db
Santiago Limas <santiago@kinedu.com>
2021-05-28 23:43:50 -0500

    agregando accion de ejecutar

--- ios/PlusPlusC/PlusPlusC/ViewController.swift
+++ ios/PlusPlusC/PlusPlusC/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var compileButton: UIButton!
     @IBOutlet weak var runButton: UIButton!
     @IBOutlet weak var cleanButton: UIButton!
@@ -20,14 +20,14 @@ class ViewController: UIViewController {
     let APIUrl: String = "http://127.0.0.1:5000/"
     let APIUrlPOSTCompile: String = "http://127.0.0.1:5000/compileFile"
     let APIUrlPOSTRun: String = "http://127.0.0.1:5000/runFile"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
         setUpElementsUI()
     }
-
+    
     private func setUpElementsUI() {
         compileButton.layer.cornerRadius = compileButton.frame.height/2
         runButton.layer.cornerRadius = runButton.frame.height/2
@@ -40,7 +40,7 @@ class ViewController: UIViewController {
         let codeInput = String(codeInputTextView.text)
         
         guard let url = URL(string: APIUrlPOSTCompile) else {
-            print("Error: Couldn't reach Flask API")
+            print("Error: Couldn't reach Flask API: APIUrlPOSTCompile")
             return
         }
         
@@ -59,7 +59,7 @@ class ViewController: UIViewController {
                 do {
                     let json = try JSONSerialization.jsonObject(with: data, options: [])
                     if let dict = json as? [String: Any] {
-                        if let value = dict["value"] as? String {
+                        if let value = dict["compiler"] as? String {
                             DispatchQueue.main.async {
                                 self.outputTextView.text = value
                             }
@@ -75,10 +75,45 @@ class ViewController: UIViewController {
     }
     
     @IBAction func runButtonPressed(_ sender: Any) {
-                guard let url = URL(string: APIUrlPOSTRun) else {
-                    print("Error: Couldn't reach Flask API")
-                    return
+        guard let url = URL(string: APIUrlPOSTRun) else {
+            print("Error: Couldn't reach Flask API: APIUrlPOSTRun")
+            return
+        }
+        
+        let outputResult = String(outputTextView.text)
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        
+        let params = ["compilerResult": outputResult]
+        
+        do {
+            let data = try JSONSerialization.data(withJSONObject: params, options: .init())
+            urlRequest.httpBody = data
+            urlRequest.setValue("application/json", forHTTPHeaderField: "content-type")
+            
+            URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+                guard let data = data else { return }
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    if let dict = json as? [String: Any] {
+                        if let value = dict["result"] as? String {
+                            self.outputTextView.text = ""
+                            DispatchQueue.main.async {
+                                if value == "" {
+                                    print("viene vacio")
+                                }
+                                self.outputTextView.text = value
+                            }
+                        }
+                    }
+                } catch {
+                    print("Error fetching key: value")
                 }
+            }.resume()
+        } catch {
+            print("Error fetching data.")
+        }
     }
     
     @IBAction func cleanButtonPressed(_ sender: Any) {
