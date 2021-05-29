0a359c619cac317b4f62026320acc964470b30e3
Santiago Limas <santiago@kinedu.com>
2021-05-28 21:07:59 -0500

    set endpoint para compilar

--- PlusPlusC/app.py
+++ PlusPlusC/app.py
@@ -1,6 +1,34 @@
-from flask import Flask, request, json
+
+import sys
+sys.path.insert(0, '.')
+
+from flask import Flask, request, json, jsonify
+from compiler.lexer.lexer import PlusPlusCLexer
+from compiler.parser.parser import PlusPlusCParser
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello, World!'
+    return {"value": "Hello, World!"}
+
+@app.route('/compileFile', methods=['POST'])
+def compile_File():
+    lexer = PlusPlusCLexer()
+    parser = PlusPlusCParser()
+    try: 
+        req_data = request.get_json()
+        file_text = req_data["code"]
+        
+        print("input_text", file_text)
+
+        text = file_text
+        parser.parse(lexer.tokenize(text))
+        compiler_output = parser.output
+
+        print("output_text", compiler_output)
+
+        return {"value": str(compiler_output)}
+    except:
+        return 'Error: La key code no funciono', 400
