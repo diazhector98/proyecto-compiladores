df052b7a84b6fcb9210bffd19edece631acd7007
diazhector98 <diazhector98@gmail.com>
2021-06-01 12:33:52 -0500

    aceptar nombre de archivo de compilacion para ejecutar programa

--- PlusPlusC/run.py
+++ PlusPlusC/run.py
@@ -6,15 +6,27 @@ from virtual_machine.file_reader import FileReader
 from virtual_machine.virtual_machine import VirtualMachine
 from virtual_machine import virtual_machine
 
-
-cwd = os.getcwd()
-file_path = os.path.join(cwd, 'PlusPlusC/output.txt')
-print(file_path)
-virtual_machine = VirtualMachine(file_path)
-virtual_machine.run()
-
 def run_virtual_machine(file_result):
     virtual_machine = VirtualMachine(file_result, read_file=False, terminal=False)
     virtual_machine.run()
     virtual_machine_output = virtual_machine.global_output_variable
     return virtual_machine_output
+
+def get_absolute_path(path):
+    if os.path.isabs(path):
+        return path
+    
+    cwd = os.getcwd()
+    file_path = os.path.join(cwd, path)
+    return file_path
+
+if __name__ == '__main__':
+    arguments = sys.argv
+    input_file_name = "output.txt"
+
+    if len(arguments) == 2:
+        input_file_name = arguments[1]
+    
+    input_abs_path = get_absolute_path(input_file_name)
+    virtual_machine = VirtualMachine(input_abs_path)
+    virtual_machine.run()
